dd7256722ad8d9e3a7e95e55d5afa3e04fcc5d36
Billy Gene Ridgeway <37519033+Billoncho@users.noreply.github.com>
2019-08-04 12:06:55 +0800

    Add files via upload

---
+++ ThankYou.py
@@ -0,0 +1,11 @@
+# ThankYou.py
+# Billy Ridgeway
+# Input practice for the Python course.
+
+my_name = "Billy"
+my_age = "?"
+your_name = input("What is your name? ")
+your_age = input("How old are you? ")
+print("My name is", my_name, ", and I am", my_age, "years old.")
+print("Your name is", your_name, ", and your are", your_age, "years old.")
+print("Thank you for taking this course", your_name, "!")
